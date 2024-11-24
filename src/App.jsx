12b13cf41c83e3db05295d83411e09b4dc901c0d
tahiee <niazi.tahirkhan15@gmail.com>
2024-11-25 03:23:57 +0500

    update final code

--- src/App.jsx
+++ src/App.jsx
@@ -2,56 +2,32 @@ import { useEffect, useState } from "react";
 
 function App() {
   const [loading, setLoading] = useState(false);
-  const [baseCurrency, setBaseCurrency] = useState("");
+  const [selectedCurrency, setSelectedCurrency] = useState("");
   const [amount, setAmount] = useState();
-  const [targetCurrencies, setTargetCurrencies] = useState([]);
-  const [conversionResults, setConversionResults] = useState([]);
-
-  // console.log(conversionResults, "these are conversion result");
-  
-  const [selectedCurrecny, setSelectedCurrecny] = useState("");
-  const [searchQuery, setSearchQuery] = useState(""); // New state for search query
-
+  const [currencies, setCurrencies] = useState([]);
+  const [searchQuery, setSearchQuery] = useState("");
+  const [convertedData, setConvertedData] = useState([]);
+  const [filterData, setFilterData] = useState([]);
   const backendUrl =
     import.meta.env.NEXT_PUBLIC_BACKEND_URL ||
     "https://smartcurrecnyconverter-backend-production.up.railway.app";
-  console.log(backendUrl, "yea backend url hai");
-
-  const [history, setHistory] = useState(() => {
-    const saved = localStorage.getItem("currencyHistory");
-    return saved ? JSON.parse(saved) : [];
-  });
-
-  // yahn data filter kiya hai
-  const filteredData = conversionResults.reStructData
-    ?.filter((result) =>
-      result.currency.toLowerCase().includes(searchQuery.toLowerCase())
-    )
-    .map((result) => ({
-      ...result,
-      amount: parseFloat(result.amount).toFixed(3), // 3 show krny hen decimal may
-    }));
 
   useEffect(() => {
-    // currencies fetch krny hen jb component mount hoga
     fetch(`${backendUrl}/api/currencies`, {
       method: "GET",
     })
       .then((res) => {
-        console.log("rew response ka masla hai", res);
         if (!res.ok) {
           throw new Error(`HTTP error! Status yahn hai 37: ${res.status}`);
         }
         return res.json();
       })
       .then(({ data }) => {
-        console.log(data);
         const reStructData = [];
-        for (const [key, value] of Object.entries(data)) {
-          console.log(`${key}: ${value}`);
+        for (const [_, value] of Object.entries(data)) {
           reStructData.push(value);
         }
-        setTargetCurrencies(reStructData); // yahn Assume krna hai curriency arry may ha
+        setCurrencies(reStructData); // yahn Assume krna hai curriency arry may ha
       })
 
       .catch((error) =>
@@ -61,14 +37,19 @@ function App() {
 
   useEffect(() => {
     // save krna hai history may jb be localstorage change hoga
-    localStorage.setItem("currencyHistory", JSON.stringify(history));
-  }, [history]);
+    const holdaValue = JSON.parse(localStorage.getItem("data"));
+    if (holdaValue) {
+      setAmount(holdaValue?.amount);
+      setSelectedCurrency(holdaValue?.base);
+      setConvertedData(holdaValue.reStructData);
+    }
+  }, []);
 
   const handleConvert = () => {
     setLoading(true);
 
     console.log("Sending request:", {
-      base: baseCurrency,
+      base: selectedCurrency,
       amount: amount,
     });
 
@@ -76,30 +57,20 @@ function App() {
       method: "POST",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
-        base: baseCurrency,
+        base: selectedCurrency,
         amount: amount,
       }),
     })
       .then((res) => {
-        console.log("Raw response: hai yea res ka", res);
         return res.json();
       })
       .then((data) => {
-        console.log(data, "yeah ha data");
-
-        setConversionResults(data); // Assuming kr rha hn data ko convert values may!
+        setConvertedData(data.reStructData);
+        // setConversionResults(data); // Assuming kr rha hn data ko convert values may!
         setLoading(false);
 
-        // Save this conversion in history
-        setHistory((prevHistory) => [
-          ...prevHistory,
-          {
-            baseCurrency,
-            amount,
-            conversionResults: data,
-            date: new Date().toISOString(),
-          },
-        ]);
+        const localData = { ...data, date: new Date().toISOString() };
+        localStorage.setItem("data", JSON.stringify(localData));
       })
       .catch((error) => {
         console.error("Error fetching conversion:", error);
@@ -110,10 +81,10 @@ function App() {
   return (
     <div className="container p-4 sm:p-6 md:w-1/2 lg:w-1/3 mx-auto">
       <div className="projectName">
-        <p className="title">SMART {' '}
-        <p className="title2">CURRENCY</p>
+        <p className="title">
+          SMART <p className="title2">CURRENCY</p>
         </p>
-        
+
         <p className="subtitle">CONVERTER</p>
       </div>
 
@@ -122,10 +93,9 @@ function App() {
           How Much are
           <br />
           <span className="mr-2">
-            {amount} {baseCurrency}{" "}
+            {amount} {selectedCurrency}{" "}
           </span>
           in
-          <span>{selectedCurrecny ? `: ${selectedCurrecny}` : "Convert"}</span>
         </div>
       </div>
 
@@ -138,12 +108,12 @@ function App() {
         </label>
         <select
           id="base-currency"
-          value={baseCurrency}
-          onChange={(e) => setBaseCurrency(e.target.value)}
+          value={selectedCurrency}
+          onChange={(e) => setSelectedCurrency(e.target.value)}
           className="w-full p-2 border rounded-md mb-4"
         >
-          {targetCurrencies.length &&
-            targetCurrencies.map((currency, index) => (
+          {currencies.length &&
+            currencies.map((currency, index) => (
               <option key={index} value={currency.code}>
                 {currency.symbol_native} {currency.code} - {currency.name}
               </option>
@@ -158,10 +128,9 @@ function App() {
           value={amount}
           onChange={(e) => setAmount(e.target.value)}
           placeholder={
-            baseCurrency &&
+            selectedCurrency &&
             `${
-              targetCurrencies.find((c) => c.code === baseCurrency)
-                ?.symbol_native
+              currencies.find((c) => c.code === selectedCurrency)?.symbol_native
             }`
           }
           className="w-full p-2 border rounded-md mb-4"
@@ -175,54 +144,54 @@ function App() {
           {loading ? "Converting..." : "CONVERT"}
         </button>
 
-        <div className="mt-4">
+        <div className="listing">
           <div className="search-bar mb-4">
             <input
               type="text"
               placeholder="Search Currency"
               value={searchQuery}
-              onChange={(e) => setSearchQuery(e.target.value)} 
-              // Update search query state
+              onChange={(e) => {
+                const filterData = convertedData?.filter((obj) =>
+                  obj.currency
+                    .toLowerCase()
+                    .includes(e.target.value.toLowerCase())
+                );
+                setFilterData(filterData);
+                setSearchQuery(e.target.value);
+              }}
               className="w-full p-2 border rounded-md"
             />
           </div>
-
           <ul className="currency-list max-h-60 overflow-y-auto border rounded-md p-2">
-            {filteredData?.length > 0 ? (
-              filteredData.map((result, index) => (
-                <li
-                  key={index}
-                  className="currency-item flex justify-between items-center p-2"
-                >
-                  <img
-                    src={`flag-${result.currency}.png`}
-                    alt={result.currency}
-                    className="currency-flag w-6 h-6 mr-2"
-                  />
-                  <span
-                    onClick={() =>
-                      setSelectedCurrecny(
-                        `${result.currency}, ${result.amount}`
-                      )
-                    }
-                    className="cursor-pointer"
+            {searchQuery?.length
+              ? filterData?.map((result, index) => (
+                  <li
+                    key={index}
+                    className="currency-item flex justify-between items-center p-2"
                   >
-                    {conversionResults.base} {conversionResults.amount} -{" "}
-                    {result.currency}: {result.amount}
-                  </span>
-                  <button
-                    onClick={() => {
-                      console.log(`Remove ${result.currency}`);
-                    }}
-                    className="text-red-500"
+                    <img
+                      src={`flag-${result.currency}.png`}
+                      alt={result.currency}
+                      className="currency-flag w-6 h-6 mr-2"
+                    />
+                    <span className="cursor-pointer">
+                      {result.currency}: {Number(result?.amount).toFixed(2)}
+                    </span>
+                  </li>
+                ))
+              : convertedData?.map((result, index) => (
+                  <li
+                    key={index}
+                    className="currency-item flex justify-between items-center p-2"
                   >
-                    X
-                  </button>
-                </li>
-              ))
-            ) : (
-              <p>No currency data available.</p>
-            )}
+                    <p>{result.symbol_native}</p>
+                    <span
+                      className="cursor-pointer"
+                    >
+                      {result.currency}: {Number(result?.amount).toFixed(2)}
+                    </span>
+                  </li>
+                ))}
           </ul>
         </div>
       </div>
